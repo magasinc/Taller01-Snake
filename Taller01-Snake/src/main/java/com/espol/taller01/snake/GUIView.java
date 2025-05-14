591b62f2403d899be28686fe5e971179b971a689
Steven Avelino <>
2025-05-14 14:25:42 -0500

    Steven Avelino
    Cambios Realizados

--- Taller01-Snake/src/main/java/com/espol/taller01/snake/GUIView.java
+++ Taller01-Snake/src/main/java/com/espol/taller01/snake/GUIView.java
@@ -16,7 +16,7 @@ import javax.swing.JPanel;
  * a GameFactory.
  */
 public class GUIView extends JPanel {
-	/** The "Start Game" button */
+	/** The "Let’s Go!!!" button */
 	private final JButton startGameButton;
 
 	/** The chooser (also called drop-down menu) with names of different games */
